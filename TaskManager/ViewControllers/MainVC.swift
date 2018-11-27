06d783352a0fd1aee5045bc2c877781270442373
Tanner York <tannerwyork@gmail.com>
2018-11-26 21:40:44 -0600

    AddSettings

---
+++ TaskManager/ViewControllers/MainVC.swift
@@ -0,0 +1,27 @@
+//
+//  MainVC.swift
+//  TaskManager
+//
+//  Created by Tanner York on 11/26/18.
+//  Copyright © 2018 Tanner York. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+    
+    //MARK: Properties
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    //MARK: Actions
+    @IBAction func backgroundRedSlider(_ sender: Any) {
+    }
+    
+
+
+}
